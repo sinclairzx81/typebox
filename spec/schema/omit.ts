c8a9130998b98cae4ddc21cd4f46a07b2d101116
Derek Wickern <dwickern@gmail.com>
2021-04-12 20:24:33 -0700

    Override existing options from a specialized schema (#69)

--- spec/schema/omit.ts
+++ spec/schema/omit.ts
@@ -1,5 +1,6 @@
 import { Type } from '@sinclair/typebox'
 import { ok, fail } from './validate'
+import { strictEqual } from 'assert'
 
 describe('Omit', () => {
     it('Vector3 to Vector2', () => {
@@ -20,5 +21,16 @@ describe('Omit', () => {
       });
       const PartialUser = Type.Omit(User, ['id'])
       ok(PartialUser, { name: 'user', email: 'user@example.com' })
+    })
+
+    it('Options', () => {
+        const Vector3 = Type.Object({
+            x: Type.Number(),
+            y: Type.Number(),
+            z: Type.Number()
+        }, { title: 'Vector3' })
+        const Vector2 = Type.Omit(Vector3, ['z'], { title: 'Vector2' })
+        strictEqual(Vector3.title, 'Vector3')
+        strictEqual(Vector2.title, 'Vector2')
     })
 })
