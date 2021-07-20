eb68e659f8eb37e2a9bf8b3d5455fe6e185a11eb
sinclairzx81 <haydn.developer@gmail.com>
2021-07-20 23:19:59 +0900

    update tests

---
+++ spec/static/optional.ts
@@ -0,0 +1,19 @@
+import { Type, Static } from '@sinclair/typebox'
+
+// --------------------------------------------
+
+const T0 = Type.Object({
+    a: Type.Number(),
+    b: Type.Optional(Type.Number())
+})
+
+const F0 = (arg: Static<typeof T0>) => {}
+
+F0({
+    a: 1,
+    b: 1,
+})
+
+F0({
+    a: 1
+})
