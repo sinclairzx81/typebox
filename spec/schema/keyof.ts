a324368c5a814a51759588f66c902f25469f00a3
sinclairzx81 <haydn.developer@gmail.com>
2021-07-20 23:04:44 +0900

    update tests

--- spec/schema/keyof.ts
+++ spec/schema/keyof.ts
@@ -6,25 +6,11 @@ describe("KeyOf", () => {
         const T = Type.KeyOf(Type.Object({
             x: Type.Number(),
             y: Type.Number(),
-            z: Type.Number(),
+            z: Type.Number()
         }))
         ok(T, 'x')
         ok(T, 'y')
         ok(T, 'z')
         fail(T, 'w')
-    })
-
-    it('Should validate ', () => {
-        const T = Type.Object({
-            k: Type.KeyOf(Type.Object({
-                x: Type.Number(),
-                y: Type.Number(),
-                z: Type.Number(),
-            }))
-        })
-        ok(T, { k: 'x' })
-        ok(T, { k: 'y' })
-        ok(T, { k: 'z' })
-        fail(T, { k: 'w' })
     })
 })
