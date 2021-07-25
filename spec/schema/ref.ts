afcb7bd2fcafd210403b7015e11d58800680d8f1
sinclairzx81 <haydn.developer@gmail.com>
2021-07-25 19:45:56 +0900

    commit

--- spec/schema/ref.ts
+++ spec/schema/ref.ts
@@ -57,4 +57,31 @@ describe('Ref', () => {
             z: 3 
         }, [])
     })
+
+    it('Should validate as a Box, and as a Ref', () => {
+        const Vertex = Type.Object({
+            x: Type.Number(),
+            y: Type.Number(),
+            z: Type.Number()
+        }, { $id: 'Vertex' })
+        
+        const Box = Type.Box({ 
+            Vertex 
+        }, { $id: 'Box' })
+        
+        const R1 = Type.Ref(Vertex)
+        
+        const R2 = Type.Ref(Box, 'Vertex')
+
+        ok(R1, { 
+            x: 1, 
+            y: 2, 
+            z: 3 
+        }, [Box])
+        ok(R2, { 
+            x: 1, 
+            y: 2, 
+            z: 3 
+        }, [Box])
+    })
 })
