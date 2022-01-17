b7a9c23bb9a1db9b5d61965ddf4f03d582d371cf
sinclairzx81 <haydn.developer@gmail.com>
2022-01-18 01:53:53 +0900

    Additional Union Test

--- spec/schema/union.ts
+++ spec/schema/union.ts
@@ -21,6 +21,14 @@ describe('Union', () => {
         ok(T, { b: 'world' })
     })
 
+    it('Should fail to validate for descriminated union types', () => {
+        const A = Type.Object({ kind: Type.Literal('A'), value: Type.String() })
+        const B = Type.Object({ kind: Type.Literal('B'), value: Type.Number() })
+        const T = Type.Union([A, B])
+        fail(T, { kind: 'A', value:  42 })     // expect { kind: 'A', value: string } 
+        fail(T, { kind: 'B', value: 'hello' }) // expect { kind: 'B', value: number } 
+    })
+
     it('Should validate union of objects where properties overlap', () => {
         const A = Type.Object({ a: Type.String() }, { additionalProperties: false })
         const B = Type.Object({ a: Type.String(), b: Type.String() }, { additionalProperties: false })
