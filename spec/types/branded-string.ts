bd258ab2fe9c16649a991f61efd1f9fecce045ae
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-01-22 12:29:30 +0100

    test: add case for string brands

--- spec/types/branded-string.ts
+++ spec/types/branded-string.ts
@@ -7,4 +7,7 @@ type WrongEmail = Brand<string, 'WrongEmail'>
 // For branded types we require to pass extra constraints, otherwise it would
 // make no sense to use them at all.
 Spec.expectType<Email>(Spec.infer(Type.BrandedString<Email>({ format: 'email' })))
+
+// We validate that brands clearly distinguish between different types.
 Spec.expectNotType<Email>(Spec.infer(Type.BrandedString<WrongEmail>({ format: 'email' })))
+Spec.expectNotType<Email>(Spec.infer(Type.String()))
