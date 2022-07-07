aa825e5f199e7fcb869367e9a20f1e179d6592db
sinclairzx81 <haydn.developer@gmail.com>
2022-07-07 15:25:52 +0900

    updates

--- example/index.ts
+++ example/index.ts
@@ -2,14 +2,10 @@ import { TypeCompiler } from '@sinclair/typebox/compiler'
 import { Value } from '@sinclair/typebox/value'
 import { Type } from '@sinclair/typebox'
 
-const T = Type.Array(Type.Number(), { uniqueItems: true, minItems: 50 })
+const T = Type.Object({
+  x: Type.String(),
+  y: Type.String(),
+  z: Type.String()
+}, { $id: 'T' })
 
-const V = [0, 1, 2, 2]
-
-const C = TypeCompiler.Compile(T)
-console.log(C.Code())
-console.log(C.Check([1, 1]))
-
-for(const error of Value.Errors(T, V)) {
-  console.log(error)
-}
+console.log(T)
