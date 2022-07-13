ab0510af2c1a443c72c758c830979a00ee64b39c
sinclairzx81 <haydn.developer@gmail.com>
2022-07-13 13:18:12 +0900

    updates

--- example/index.ts
+++ example/index.ts
@@ -1,14 +1,28 @@
 import { TypeCompiler } from '@sinclair/typebox/compiler'
 import { Conditional } from '@sinclair/typebox/conditional'
 import { Value } from '@sinclair/typebox/value'
-import { Type, Static } from '@sinclair/typebox'
+import { Type, Static, TObject, TOmit, TUnion, TTuple, TLiteral, ObjectOptions, TPick, ObjectPropertyKeys, UnionToTuple } from '@sinclair/typebox'
 
-const T = Type.Object({
+
+const S = Type.Object({
+  x: Type.Number(),
+  y: Type.Number(),
+  z: Type.Number(),
+  a: Type.Number(),
+  b: Type.Number(),
+  c: Type.Number()
+})
+
+const A = Type.Object({
   x: Type.Number(),
   y: Type.Number(),
   z: Type.Number()
 })
 
-type T = Static<typeof T>
+
+
+const T = Type.Omit(S, Type.KeyOf(A))
 
 console.log(T)
+
+type T = Static<typeof T>
