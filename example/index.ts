c39a471c662bc352eae2a40bd025c05e5ed60deb
sinclairzx81 <haydn.developer@gmail.com>
2022-09-05 00:58:05 +0900

    updates

--- example/index.ts
+++ example/index.ts
@@ -6,36 +6,12 @@ import { Format } from '@sinclair/typebox/format'
 import { Value } from '@sinclair/typebox/value'
 import { Type, Static } from '@sinclair/typebox'
 
-const Player = Type.Object({
-  id: Type.String(),
+const T = Type.Object({
   x: Type.Number(),
-  y: Type.Number()
+  y: Type.Number(),
+  z: Type.Number()
 })
 
+type T = Static<typeof T>
 
-const Game = Type.Object({
-  players: Type.Array(Player)
-})
-
-
-// initial game state
-const gamestate1 = Value.Create(Game)
-
-// next game state
-const gamestate2 = Value.Clone(gamestate1)
-gamestate2.players.push({...Value.Create(Player), id: '1' })
-gamestate2.players.push({...Value.Create(Player), id: '2' })
-
-// compute delta and send across network
-
-const delta = Value.Diff(gamestate1, gamestate2)
-
-// apply delta to remote gamestate
-
-const gamestate3 = Value.Patch(gamestate1, delta)
-
-// expect equal
-
-const equal = Value.Equal(gamestate2, gamestate3)
-
-console.log(equal)
+console.log(T)
