90e55b9b7bfb9392ede9983cdf6b0e402631b5d7
sinclairzx81 <haydn.developer@gmail.com>
2022-09-05 00:33:46 +0900

    updates

--- example/index.ts
+++ example/index.ts
@@ -1,9 +1,17 @@
+import { TypeBoxCodegen } from '@sinclair/typebox/codegen'
 import { TypeCompiler } from '@sinclair/typebox/compiler'
-import { Type, Static } from '@sinclair/typebox'
+import { Conditional } from '@sinclair/typebox/conditional'
+import { TypeGuard } from '@sinclair/typebox/guard'
+import { Format } from '@sinclair/typebox/format'
 import { Value } from '@sinclair/typebox/value'
+import { Type, Static } from '@sinclair/typebox'
 
-const T = Type.Object({ x: Type.Number(), y: Type.Number() })
+const T = Type.Object({
+  x: Type.Number(),
+  y: Type.Number(),
+  z: Type.Number()
+})
 
-const R = [...Value.Errors(T, { x: '42' })]                   // const R = true
+type T = Static<typeof T>
 
-console.log(R)
+console.log(T)
