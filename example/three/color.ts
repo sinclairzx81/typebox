72729e55909ab4f5e16a3d33dda8c192d2300142
sinclairzx81 <haydn.developer@gmail.com>
2022-09-09 07:41:15 +0900

    updates

--- example/three/color.ts
+++
@@ -1,4 +0,0 @@
-import { Type, Static, SchemaOptions } from '@sinclair/typebox'
-
-export type Color = Static<ReturnType<typeof Color>>
-export const Color = (options: SchemaOptions = {}) => Type.String({ ...options, $id: 'Color' })
