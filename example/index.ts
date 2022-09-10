98e1afa09a2a131306d4b982dd5676ef2897f56d
sinclairzx81 <haydn.developer@gmail.com>
2022-09-11 05:57:19 +0900

    updates

--- example/index.ts
+++ example/index.ts
@@ -6,11 +6,12 @@ import { Format } from '@sinclair/typebox/format'
 import { Value } from '@sinclair/typebox/value'
 import { Type, Static } from '@sinclair/typebox'
 
-const T = Type.Array(Type.Number(), {
-  minItems: 5,
-  uniqueItems: true
+const T = Type.Object({
+  x: Type.Number(),
+  y: Type.Number(),
+  z: Type.Number()
 })
 
-const C = Value.Cast(T, [0, 0, 0])
+type T = Static<typeof T>
 
-console.log(C)
+console.log(T)
