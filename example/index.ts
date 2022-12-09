596d5fd9b46ed0d6925f3291cc981cb001b169dc
sinclair <haydn.developer@gmail.com>
2022-12-09 20:59:37 +0900

    reset example

--- example/index.ts
+++ example/index.ts
@@ -7,9 +7,6 @@ import { Custom } from '@sinclair/typebox/custom'
 import { Value, ValuePointer } from '@sinclair/typebox/value'
 import { Type, Kind, Static, TSchema } from '@sinclair/typebox'
 
-console.log(Value.Hash(1))
-console.log(Value.Hash(2))
-
 const T = Type.Object({
   x: Type.Number(),
   y: Type.Number(),
@@ -18,3 +15,4 @@ const T = Type.Object({
 
 type T = Static<typeof T>
 
+console.log(T)
