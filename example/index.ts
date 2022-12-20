906a1013f8a2b2bc446607ab483926f4d86923cf
sinclair <haydn.developer@gmail.com>
2022-12-20 11:42:25 +0900

    reset example

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,19 @@
 import { CodeGen } from '@sinclair/typebox/codegen'
-import { TypeCompiler } from '@sinclair/typebox/compiler'
-import { Conditional } from '@sinclair/typebox/conditional'
-import { TypeGuard } from '@sinclair/typebox/guard'
 import { TypeSystem } from '@sinclair/typebox/system'
+import { TypeCompiler } from '@sinclair/typebox/compiler'
+import { TypeGuard } from '@sinclair/typebox/guard'
+import { Conditional } from '@sinclair/typebox/conditional'
 import { Format } from '@sinclair/typebox/format'
 import { Custom } from '@sinclair/typebox/custom'
 import { Value, ValuePointer } from '@sinclair/typebox/value'
 import { Type, Kind, Static, TSchema } from '@sinclair/typebox'
 
-TypeSystem.Kind = 'structural'
+const T = Type.Object({
+    x: Type.Number(),
+    y: Type.Number(),
+    z: Type.Number(),
+  })
+  
+  type T = Static<typeof T>
+  
+  console.log(T)
