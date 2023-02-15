f6b9da6291f805886280ad6b9aec13cadbd90224
sinclair <haydn.developer@gmail.com>
2023-02-15 14:01:43 +0900

    identifier

--- example/index.ts
+++ example/index.ts
@@ -8,12 +8,39 @@ import { Custom } from '@sinclair/typebox/custom'
 import { Value, ValuePointer } from '@sinclair/typebox/value'
 import { Type, Kind, Static, TSchema } from '@sinclair/typebox'
 
-const T = Type.Object({
-  x: Type.Number(),
-  y: Type.Number(),
-  z: Type.Number(),
-})
+const Math = {
+  $id: 'https://domain.com/math',
+  Vector4: Type.Object(
+    {
+      x: Type.Number(),
+      y: Type.Number(),
+      z: Type.Number(),
+      w: Type.Number(),
+    },
+    { $id: 'Vector4' },
+  ),
+  Vector3: Type.Object(
+    {
+      x: Type.Number(),
+      y: Type.Number(),
+      z: Type.Number(),
+    },
+    { $id: 'Vector3' },
+  ),
+  Vector2: Type.Object(
+    {
+      x: Type.Number(),
+      y: Type.Number(),
+    },
+    { $id: '유니코드를 지원해야 합니다' },
+  ),
+}
 
-type T = Static<typeof T>
+const C = TypeCompiler.Compile(Math.Vector2)
 
-console.log(T)
+console.log(C.Code())
+
+console.log(C.Check({
+  x: 1,
+  y: 2
+}))
