5e2452901a96de762a823d9f8921acb86664fba7
sinclair <haydn.developer@gmail.com>
2023-02-15 15:50:59 +0900

    reset example

--- example/index.ts
+++ example/index.ts
@@ -8,36 +8,12 @@ import { Custom } from '@sinclair/typebox/custom'
 import { Value, ValuePointer } from '@sinclair/typebox/value'
 import { Type, Kind, Static, TSchema } from '@sinclair/typebox'
 
-const Vector = Type.Object(
-  {
-    x: Type.Number(),
-    y: Type.Number(),
-    z: Type.Number(),
-  },
-  {
-    $id: 'http://domain.com/schemas/Vector',
-  },
-)
+const T = Type.Object({
+  x: Type.Number(),
+  y: Type.Number(),
+  z: Type.Number(),
+})
 
-const C = TypeCompiler.Compile(Vector)
+type T = Static<typeof T>
 
-console.log(C.Code())
-
-// function check_http_58_47_47_domain_46_com_47_schemas_47_Vector(value) {
-//   return (
-//     (typeof value === 'object' && value !== null && !Array.isArray(value)) &&
-//     (typeof value.x === 'number' && !isNaN(value.x)) &&
-//     (typeof value.y === 'number' && !isNaN(value.y)) &&
-//     (typeof value.z === 'number' && !isNaN(value.z))
-//  )
-// }
-// return function check(value) {
-//   return (
-//     (check_http_58_47_47_domain_46_com_47_schemas_47_Vector(value))
-//  )
-// }
-
-console.log(String.fromCharCode(58)) // :
-console.log(String.fromCharCode(47)) // /
-console.log(String.fromCharCode(47)) // /
-console.log(String.fromCharCode(46)) // .
+console.log(T)
