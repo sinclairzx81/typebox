ecc2ab10778f51204f878ae9b97bda675d31baaf
sinclair <haydn.developer@gmail.com>
2023-03-28 23:42:55 +0900

    record and union conversion logic

--- test/runtime/value/convert/union.ts
+++ test/runtime/value/convert/union.ts
@@ -3,5 +3,22 @@ import { Type } from '@sinclair/typebox'
 import { Assert } from '../../assert/index'
 
 describe('value/convert/Union', () => {
-  it('Should convert', () => {})
+  it('Should convert union variant', () => {
+    const T = Type.Object({
+      x: Type.Union([Type.Number(), Type.Null()]),
+    })
+    const V1 = Value.Convert(T, { x: '42' })
+    const V2 = Value.Convert(T, { x: 'null' })
+    const V3 = Value.Convert(T, { x: 'hello' })
+    Assert.deepEqual(V1, { x: 42 })
+    Assert.deepEqual(V2, { x: null })
+    Assert.deepEqual(V3, { x: 'hello' })
+  })
+  it('Should convert first variant in ambigous conversion', () => {
+    const T = Type.Object({
+      x: Type.Union([Type.Boolean(), Type.Number()]),
+    })
+    const V1 = Value.Convert(T, { x: '1' })
+    Assert.deepEqual(V1, { x: true })
+  })
 })
