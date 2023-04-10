691c24b845a749b46734724a44e79c8b251e37e1
sinclair <haydn.developer@gmail.com>
2023-04-10 20:27:53 +0900

    Revision 0.27.1

--- test/runtime/value/mutate/mutate.ts
+++ test/runtime/value/mutate/mutate.ts
@@ -29,58 +29,58 @@ describe('value/mutate/Mutate', () => {
   // Mutate
   // --------------------------------------------
   it('Should mutate 0', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, {})
     Assert.deepEqual(A, {})
   })
   it('Should mutate 1', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: { y: { z: 2 } } })
     Assert.equal(A.x.y.z, 2)
     Assert.equal(A.x.y, Y)
     Assert.equal(A.x, X)
   })
   it('Should mutate 2', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: { y: { z: [1, 2, 3] } } })
     Assert.deepEqual(A.x.y.z, [1, 2, 3])
     Assert.equal(A.x.y, Y)
     Assert.equal(A.x, X)
   })
   it('Should mutate 3', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: {} })
     Assert.equal(A.x.y, undefined)
     Assert.equal(A.x, X)
   })
   it('Should mutate 4', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: { y: 1 } })
     Assert.equal(A.x.y, 1)
     Assert.equal(A.x, X)
   })
   it('Should mutate 5', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: { y: [1, 2, 3] } })
     Assert.deepEqual(A.x.y, [1, 2, 3])
     Assert.equal(A.x, X)
   })
   it('Should mutate 6', () => {
-    const Y = { z: 1 } 
+    const Y = { z: 1 }
     const X = { y: Y }
-    const A = { x: X } 
+    const A = { x: X }
     Value.Mutate(A, { x: [1, 2, 3] })
     Assert.notEqual(A.x, X)
     Assert.deepEqual(A.x, [1, 2, 3])
