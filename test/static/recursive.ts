13e00a4f7be47310bc6a0bd43929d5226aaae81c
sinclair <haydn.developer@gmail.com>
2023-04-14 17:00:26 +0900

    Recursive Utility Types

--- test/static/recursive.ts
+++ test/static/recursive.ts
@@ -2,14 +2,79 @@ import { Expect } from './assert'
 import { Type, Static } from '@sinclair/typebox'
 
 {
-  const T = Type.Recursive((Node) =>
+  // identity
+  const R = Type.Recursive((Node) =>
     Type.Object({
       id: Type.String(),
       nodes: Type.Array(Node),
     }),
   )
-
-  type T = Static<typeof T>
-
-  Expect(T).ToInfer<T>() // ? how to test....
+  type T = Static<typeof R>
+  Expect(R).ToInfer<{ id: string; nodes: T[] }>()
+}
+{
+  // keyof
+  const R = Type.Recursive((Node) =>
+    Type.Object({
+      id: Type.String(),
+      nodes: Type.Array(Node),
+    }),
+  )
+  const T = Type.KeyOf(R)
+  Expect(T).ToInfer<'id' | 'nodes'>()
+}
+{
+  // partial
+  const R = Type.Recursive((Node) =>
+    Type.Object({
+      id: Type.String(),
+      nodes: Type.Array(Node),
+    }),
+  )
+  const T = Type.Partial(R)
+  Expect(T).ToInfer<{
+    id: string | undefined
+    nodes: Static<typeof T>[] | undefined
+  }>()
+}
+{
+  // required
+  const R = Type.Recursive((Node) =>
+    Type.Object({
+      id: Type.String(),
+      nodes: Type.Array(Node),
+    }),
+  )
+  const P = Type.Partial(R)
+  const T = Type.Required(P)
+  Expect(T).ToInfer<{
+    id: string
+    nodes: Static<typeof T>[]
+  }>()
+}
+{
+  // pick
+  const R = Type.Recursive((Node) =>
+    Type.Object({
+      id: Type.String(),
+      nodes: Type.Array(Node),
+    }),
+  )
+  const T = Type.Pick(R, ['id'])
+  Expect(T).ToInfer<{
+    id: string
+  }>()
+}
+{
+  // omit
+  const R = Type.Recursive((Node) =>
+    Type.Object({
+      id: Type.String(),
+      nodes: Type.Array(Node),
+    }),
+  )
+  const T = Type.Omit(R, ['id'])
+  Expect(T).ToInfer<{
+    nodes: Static<typeof T>[]
+  }>()
 }
