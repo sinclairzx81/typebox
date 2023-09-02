9b0ba29859ff4b964c28a0f57bca83b4a1c2886a
dippi <angelo.dipilla@mail.polimi.it>
2023-09-02 19:43:53 +0200

    Add complex constraint

--- test/static/assert.ts
+++ test/static/assert.ts
@@ -1,6 +1,7 @@
 import { Static, StaticDecode, StaticEncode, TSchema } from '@sinclair/typebox'
 
 declare const unsatisfiable: unique symbol
+// Warning: `never` and `any` satisfy the constraint `extends Expected<...>`
 type Expected<_> = { [unsatisfiable]: never; }
 
 type ConstrainEqual<T, U> = (
@@ -13,9 +14,35 @@ type ConstrainEqual<T, U> = (
 type CircularHelper<T, U> = [T] extends U ? T : Expected<T>
 type ConstraintMutuallyExtend<T, U> = CircularHelper<T, [U]>
 
-// Comment one or the other to see the difference
+type If<T, Y, N> = T extends true ? Y : N
+type And<T, U> = If<T, U, false>
+type Or<T, U> = If<T, true, U>
+type Not<T> = If<T, false, true>
+
+type IsAny<a> = 0 extends 1 & a ? true : false
+type Extends<T, U> = [T] extends [U] ? true : false
+
+// If U is never, there's nothing we can do
+type ComplexConstraint<T, U> = If<
+  // If U is any, we can't use Expect<T> or it would satisfy the constraint
+  And<Not<IsAny<T>>, IsAny<U>>,
+  never,
+  If<
+    Or<
+      // If they are both any we are happy
+      And<IsAny<T>, IsAny<U>>,
+      // If T extends U, but not because it's any, we are happy
+      And<Extends<T, U>, Not<IsAny<T>>>
+    >,
+    T,
+    Expected<T>
+  >
+>
+
+// Uncomment one of the lines to see the difference
 // type Constraint<T, U> = ConstraintMutuallyExtend<T, U>
-type Constraint<T, U> = ConstrainEqual<T, U>
+// type Constraint<T, U> = ConstrainEqual<T, U>
+type Constraint<T, U> = ComplexConstraint<T, U>
 
 export function Expect<T extends TSchema>(schema: T) {
   return {
