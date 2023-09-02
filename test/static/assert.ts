a730d9a9a245259320becc16c936054cbabc66ef
dippi <angelo.dipilla@mail.polimi.it>
2023-09-02 17:36:44 +0200

    Improve assertion debugging showing the expected type in the error

--- test/static/assert.ts
+++ test/static/assert.ts
@@ -1,13 +1,16 @@
 import { Static, StaticDecode, StaticEncode, TSchema } from '@sinclair/typebox'
 
+declare const unsatisfiable: unique symbol
+type Expected<_> = { [unsatisfiable]: never; }
+
 type ConstrainEqual<T, U> = (
   <V>() => V extends T ? 1 : 2
 ) extends <V>() => V extends U ? 1 : 2
   ? T
-  : never
+  : Expected<T>
 
 // See https://github.com/microsoft/TypeScript/issues/51011
-type CircularHelper<T, U> = [T] extends U ? T : never
+type CircularHelper<T, U> = [T] extends U ? T : Expected<T>
 type ConstraintMutuallyExtend<T, U> = CircularHelper<T, [U]>
 
 // Comment one or the other to see the difference
