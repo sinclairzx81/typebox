a7137a3d934bfd54909656a0975c511114687ad3
sinclair <haydn.developer@gmail.com>
2023-09-06 02:46:07 +0900

    Update Test To Include Non Converted Property

--- test/runtime/value/convert/intersect.ts
+++ test/runtime/value/convert/intersect.ts
@@ -22,7 +22,7 @@ describe('value/convert/Intersect', () => {
     const R = Value.Convert(T, { x: '1', y: '2' })
     Assert.IsEqual(R, { x: '1', y: '2' })
   })
-  it('Should convert first value for object exclusive intersect 1', () => {
+  it('Should convert first type for object exclusive intersect 1', () => {
     // prettier-ignore
     const T = Type.Intersect([
       Type.Number(),
@@ -32,14 +32,14 @@ describe('value/convert/Intersect', () => {
     const R = Value.Convert(T, '123')
     Assert.IsEqual(R, 123)
   })
-  it('Should convert first value for object exclusive intersect 2', () => {
+  it('Should convert first type for object exclusive intersect 2', () => {
     // prettier-ignore
     const T = Type.Intersect([
       Type.Object({ x: Type.Number() }),
       Type.Object({ y: Type.Number() }),
       Type.Number(),
     ])
-    const R = Value.Convert(T, { x: '3' })
-    Assert.IsEqual(R, { x: 3 })
+    const R = Value.Convert(T, { x: '3', y: '4' })
+    Assert.IsEqual(R, { x: 3, y: '4' })
   })
 })
