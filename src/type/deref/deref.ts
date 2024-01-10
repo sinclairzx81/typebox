a15483265716e63ff9c8c6fa6cf231d64aa4beb1
sinclair <haydn.developer@gmail.com>
2024-01-10 11:33:16 +0900

    Export Deref Infrastructure

--- src/type/deref/deref.ts
+++ src/type/deref/deref.ts
@@ -53,7 +53,7 @@ import { IsConstructor, IsFunction, IsIntersect, IsUnion, IsTuple, IsArray, IsOb
 // prettier-ignore
 export type TFromRest<T extends TSchema[], Acc extends TSchema[] = []> = (
   T extends [infer L extends TSchema, ...infer R extends TSchema[]]
-   ? TFromRest<R, [...Acc, DerefResolve<L>]>
+   ? TFromRest<R, [...Acc, TDeref<L>]>
    : Acc
 )
 function FromRest(schema: TSchema[], references: TSchema[]) {
@@ -64,7 +64,7 @@ function FromRest(schema: TSchema[], references: TSchema[]) {
 // ------------------------------------------------------------------
 // prettier-ignore
 type FromProperties<T extends TProperties> = Evaluate<{
-  [K in keyof T]: DerefResolve<T[K]>
+  [K in keyof T]: TDeref<T[K]>
 }>
 // prettier-ignore
 function FromProperties(properties: TProperties, references: TSchema[]) {
@@ -133,20 +133,6 @@ function FromRef(schema: TRef, references: TSchema[]) {
   return Deref(discard, references)
 }
 // prettier-ignore
-export type DerefResolve<T extends TSchema> =
-  T extends TConstructor<infer S extends TSchema[], infer R extends TSchema> ? TConstructor<TFromRest<S>, DerefResolve<R>> :
-  T extends TFunction<infer S extends TSchema[], infer R extends TSchema> ? TFunction<TFromRest<S>, DerefResolve<R>> :
-  T extends TIntersect<infer S extends TSchema[]> ? TIntersect<TFromRest<S>> :
-  T extends TUnion<infer S extends TSchema[]> ? TUnion<TFromRest<S>> :
-  T extends TTuple<infer S extends TSchema[]> ? TTuple<TFromRest<S>> :
-  T extends TObject<infer S extends TProperties> ? TObject<FromProperties<S>> :
-  T extends TArray<infer S extends TSchema> ? TArray<DerefResolve<S>> :
-  T extends TPromise<infer S extends TSchema> ? TPromise<DerefResolve<S>> :
-  T extends TAsyncIterator<infer S extends TSchema> ? TAsyncIterator<DerefResolve<S>> :
-  T extends TIterator<infer S extends TSchema> ? TIterator<DerefResolve<S>> :
-  T extends TRef<infer S extends TSchema> ? DerefResolve<S> :
-  T
-// prettier-ignore
 export function DerefResolve<T extends TSchema>(schema: T, references: TSchema[]): TDeref<T>  {
   return (
     IsConstructor(schema) ? FromConstructor(schema, references) :
@@ -163,11 +149,23 @@ export function DerefResolve<T extends TSchema>(schema: T, references: TSchema[]
     schema
   ) as TDeref<T> 
 }
+// prettier-ignore
+export type TDeref<T extends TSchema> =
+  T extends TConstructor<infer S extends TSchema[], infer R extends TSchema> ? TConstructor<TFromRest<S>, TDeref<R>> :
+  T extends TFunction<infer S extends TSchema[], infer R extends TSchema> ? TFunction<TFromRest<S>, TDeref<R>> :
+  T extends TIntersect<infer S extends TSchema[]> ? TIntersect<TFromRest<S>> :
+  T extends TUnion<infer S extends TSchema[]> ? TUnion<TFromRest<S>> :
+  T extends TTuple<infer S extends TSchema[]> ? TTuple<TFromRest<S>> :
+  T extends TObject<infer S extends TProperties> ? TObject<FromProperties<S>> :
+  T extends TArray<infer S extends TSchema> ? TArray<TDeref<S>> :
+  T extends TPromise<infer S extends TSchema> ? TPromise<TDeref<S>> :
+  T extends TAsyncIterator<infer S extends TSchema> ? TAsyncIterator<TDeref<S>> :
+  T extends TIterator<infer S extends TSchema> ? TIterator<TDeref<S>> :
+  T extends TRef<infer S extends TSchema> ? TDeref<S> :
+  T
 // ------------------------------------------------------------------
 // TDeref
 // ------------------------------------------------------------------
-export type TDeref<T extends TSchema> = DerefResolve<T>
-
 /** `[Json]` Creates a dereferenced type */
 export function Deref<T extends TSchema>(schema: T, references: TSchema[]): TDeref<T> {
   return DerefResolve(CloneType(schema), CloneRest(references))
