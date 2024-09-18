0ed2071eed41194fb1d333299c85736ef670700c
sinclairzx81 <haydn.developer@gmail.com>
2024-09-19 02:39:40 +0900

    Revision 0.33.12 (#1000)
    * Version + Formatting
    
    * ChangeLog

--- test/runtime/value/convert/bigint.ts
+++ test/runtime/value/convert/bigint.ts
@@ -26,22 +26,22 @@ describe('value/convert/BigInt', () => {
   it('Should convert bigint from string 5', () => {
     const T = Type.BigInt()
     const R = Value.Convert(T, '12345678901234567890')
-    Assert.IsEqual(R, BigInt("12345678901234567890"))
+    Assert.IsEqual(R, BigInt('12345678901234567890'))
   })
   it('Should convert bigint from string 6', () => {
     const T = Type.BigInt()
     const R = Value.Convert(T, '-12345678901234567890')
-    Assert.IsEqual(R, BigInt("-12345678901234567890"))
+    Assert.IsEqual(R, BigInt('-12345678901234567890'))
   })
   it('Should convert bigint from string 7', () => {
     const T = Type.BigInt()
     const R = Value.Convert(T, '12345678901234567890.123')
-    Assert.IsEqual(R, BigInt("12345678901234567890"))
+    Assert.IsEqual(R, BigInt('12345678901234567890'))
   })
   it('Should convert bigint from string 8', () => {
     const T = Type.BigInt()
     const R = Value.Convert(T, '-12345678901234567890.123')
-    Assert.IsEqual(R, BigInt("-12345678901234567890"))
+    Assert.IsEqual(R, BigInt('-12345678901234567890'))
   })
   it('Should convert bitint from number 1', () => {
     const T = Type.BigInt()
