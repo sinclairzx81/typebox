9a0309869dc7fbe8b71a23cd04869d318d4d11ce
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-04 22:08:04 +0100

    Fix: Converting Large Numbers to BigInt (#1019)
    * Fix converting large numbers to BigInts
    
    * Clarify numeric value
    
    * Add test

--- src/value/convert/convert.ts
+++ src/value/convert/convert.ts
@@ -120,7 +120,7 @@ function TryConvertBoolean(value: unknown) {
 }
 function TryConvertBigInt(value: unknown) {
   const truncateInteger = (value: string) => value.split('.')[0]
-  return IsStringNumeric(value) ? BigInt(truncateInteger(value)) : IsNumber(value) ? BigInt(value | 0) : IsValueFalse(value) ? BigInt(0) : IsValueTrue(value) ? BigInt(1) : value
+  return IsStringNumeric(value) ? BigInt(truncateInteger(value)) : IsNumber(value) ? BigInt(Math.trunc(value)) : IsValueFalse(value) ? BigInt(0) : IsValueTrue(value) ? BigInt(1) : value
 }
 function TryConvertString(value: unknown) {
   return IsValueToString(value) ? value.toString() : IsSymbol(value) && value.description !== undefined ? value.description.toString() : value
