199274271bf626965b681898041ada6d1d7fc498
Haydn Paterson <haydn.developer@gmail.com>
2024-11-18 01:59:25 +0900

    Revision 0.34.3 (#1083)
    * Retain Array on Value Default
    
    * Version
    
    * ChangeLog

--- test/runtime/value/default/array.ts
+++ test/runtime/value/default/array.ts
@@ -21,4 +21,30 @@ describe('value/default/Array', () => {
     const R = Value.Default(T, [1, undefined, 3])
     Assert.IsEqual(R, [1, 2, 3])
   })
+  // ----------------------------------------------------------------
+  // Elements
+  // ----------------------------------------------------------------
+  it('Should should retain array and only initialize undefined elements', () => {
+    const T = Type.Array(Type.Literal('hello', { default: 'hello' }))
+    const R = Value.Default(T, [1, undefined, 3])
+    Assert.IsEqual(R, [1, 'hello', 3])
+  })
+  // ----------------------------------------------------------------
+  // https://github.com/ubiquity-os-marketplace/command-start-stop/pull/86
+  // ----------------------------------------------------------------
+  it('Should retain arrays 1', () => {
+    const U = Type.Union([Type.Literal('A'), Type.Literal('B'), Type.Literal('C'), Type.Literal('D')], { default: 'A' })
+    const T = Type.Array(U, { default: ['A', 'B', 'C', 'D'], uniqueItems: true })
+    Assert.IsEqual(Value.Default(T, undefined), ['A', 'B', 'C', 'D'])
+    Assert.IsEqual(Value.Default(T, []), [])
+    Assert.IsEqual(Value.Default(T, ['A']), ['A'])
+    // initialize undefined element
+    Assert.IsEqual(Value.Default(T, [undefined, 'B', 'C', 'D']), ['A', 'B', 'C', 'D'])
+  })
+  it('Should retain arrays 2', () => {
+    const U = Type.Union([Type.Literal('A'), Type.Literal('B'), Type.Literal('C'), Type.Literal('D')], { default: 'A' })
+    // undefined first element initialized by union default
+    const T = Type.Array(U, { default: [undefined, 'B', 'C', 'D'], uniqueItems: true })
+    Assert.IsEqual(Value.Default(T, undefined), ['A', 'B', 'C', 'D'])
+  })
 })
