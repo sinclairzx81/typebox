29dd72570c04e55970651d5d121fe89e5e7188bb
sinclair <haydn.developer@gmail.com>
2025-10-11 19:46:08 +0900

    Revert Dual Publishing

--- tasks.ts
+++ tasks.ts
@@ -13,7 +13,7 @@ const Version = '1.0.33'
 // ------------------------------------------------------------------
 // Build
 // ------------------------------------------------------------------
-const BuildPackage = (target: string = `target/build`) => Task.build.dual('src', {
+const BuildPackage = (target: string = `target/build`) => Task.build.esm('src', {
   outdir: target,
   compiler: '5.9.2',
   additional: ['license', 'readme.md'],
