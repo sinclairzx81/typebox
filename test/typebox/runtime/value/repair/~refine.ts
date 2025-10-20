55845922a2fea1168f9b7b90a873d893b0d240a7
sinclairzx81 <haydn.developer@gmail.com>
2025-10-20 14:00:47 +0900

    Version 1.0.41 (#1416)
    * Repair: Support Sub Schema Refine Recovery
    
    * ChangeLog
    
    * Version

--- test/typebox/runtime/value/repair/~refine.ts
+++ test/typebox/runtime/value/repair/~refine.ts
@@ -13,7 +13,7 @@ Test('Should Refine 1', () => {
       greater: Type.Integer({ default: 10 }),
       smaller: Type.Integer({ default: 5 })
     }),
-    (data) => data.greater > data.smaller
+    (value) => value.greater > value.smaller
   )
   const R = Value.Repair(T, { greater: 3, smaller: 7 })
   Assert.IsEqual(R, {
@@ -32,7 +32,7 @@ Test('Should Refine 2', () => {
         smaller: 5
       }
     }),
-    (data) => data.greater > data.smaller
+    (value) => value.greater > value.smaller
   )
   const R = Value.Repair(T, { greater: 3, smaller: 7 })
   Assert.IsEqual(R, {
@@ -51,11 +51,45 @@ Test('Should Refine 3', () => {
         smaller: 50
       }
     }),
-    (data) => data.greater > data.smaller
+    (value) => value.greater > value.smaller
   )
   const R = Value.Repair(T, { greater: 3, smaller: 7 })
   Assert.IsEqual(R, {
     greater: 100,
     smaller: 50
   })
+})
+// ------------------------------------------------------------------
+// Coverage (Fall through case for when post refine-checks pass)
+// ------------------------------------------------------------------
+Test('Should Refine 4', () => {
+  const T = Type.Refine(
+    Type.Object({
+      greater: Type.Integer({ default: 10 }),
+      smaller: Type.Integer({ default: 5 })
+    }),
+    (value) => value.greater > value.smaller
+  )
+  const R = Value.Repair(T, { greater: 10, smaller: 5 })
+  Assert.IsEqual(R, {
+    greater: 10,
+    smaller: 5
+  })
+})
+// ------------------------------------------------------------------
+// https://github.com/sinclairzx81/typebox/issues/1414#issuecomment-3420056090
+// ------------------------------------------------------------------
+Test('Should Refine 5', () => {
+  const T = Type.Object({
+    bounds: Type.Refine(
+      Type.Object({
+        max: Type.Integer({ default: 10 }),
+        min: Type.Integer({ default: 5 })
+      }),
+      (value) => value.max > value.min
+    ),
+    otherData: Type.String({ default: '' })
+  })
+  const R = Value.Repair(T, { bounds: { max: 10, min: 15 }, otherData: 'keep this' })
+  Assert.IsEqual(R, { bounds: { max: 10, min: 5 }, otherData: 'keep this' })
 })
