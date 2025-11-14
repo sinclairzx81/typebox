0b634768f75c5a0dd932c4c8ffab349798f57eba
sinclair <haydn.developer@gmail.com>
2025-11-14 15:18:40 +0900

    Version

--- tasks.ts
+++ tasks.ts
@@ -8,7 +8,7 @@ import { Range } from './task/range/index.ts'
 import { Metrics } from './task/metrics/index.ts'
 import { Task } from 'tasksmith'
 
-const Version = '1.0.52'
+const Version = '1.0.53'
 
 // ------------------------------------------------------------------
 // Build
