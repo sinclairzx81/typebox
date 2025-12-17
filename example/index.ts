ac6892524292f21538fda453e3f89ce6b0cc22ce
sinclair <haydn.developer@gmail.com>
2025-12-17 13:45:18 +0900

    Notes

--- example/index.ts
+++ example/index.ts
@@ -3,12 +3,19 @@ import System from 'typebox/system'
 import Format from 'typebox/format'
 import Schema from 'typebox/schema'
 import Value from 'typebox/value'
-import Type from 'typebox'
+import * as Type from 'typebox'
+import { TSchema, TUnion } from 'typebox'
 import Guard from 'typebox/guard'
+import { TLiteral } from '../src/typebox.ts'
 
-// Remove Encode/Decode
+// TypeBox 1.1
+
+// Remove Encode / Decode
 // Remove Base
 // Remove Mutate
 // Rename ReadonlyType to ReadonlyObject
 
-
+// Todo: Distribute Parameters on TCall
+//       Rename extension keywords to `x-`
+//       Support { [key: string]: number }
+//       Support { [T, ...unknown[]] }
