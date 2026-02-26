7fbdb3012cafd204cec1bf73f1d605ce92caf1fc
sinclair <haydn.developer@gmail.com>
2026-02-26 14:31:29 +0900

    Format: Json Pointer Get

--- src/schema/pointer/pointer-get.ts
+++ src/schema/pointer/pointer-get.ts
@@ -26,33 +26,42 @@ THE SOFTWARE.
 
 ---------------------------------------------------------------------------*/
 
+// deno-fmt-ignore-file
+
 // ------------------------------------------------------------------
-// Escape
+// TEscape
 // ------------------------------------------------------------------
-type TEscape0<Index extends string> = Index extends `${infer Left}~0${infer Right}` ? `${Left}~${TEscape<Right>}`
-  : Index
-type TEscape1<Index extends string> = Index extends `${infer Left}~1${infer Right}` ? `${Left}/${TEscape<Right>}`
-  : Index
+type TEscape0<Index extends string> = Index extends `${infer Left}~0${infer Right}` ? `${Left}~${TEscape<Right>}` : Index
+type TEscape1<Index extends string> = Index extends `${infer Left}~1${infer Right}` ? `${Left}/${TEscape<Right>}` : Index
 type TEscape<Index extends string, Escaped0 extends string = TEscape0<Index>, Escaped1 extends string = TEscape1<Escaped0>> = Escaped1
 // ------------------------------------------------------------------
-// Indices
+// TIndices
 // ------------------------------------------------------------------
-type IndicesReduce<Pointer extends string, Result extends string[] = []> = Pointer extends `${infer Left extends string}/${infer Right extends string}` ? Left extends '' ? IndicesReduce<Right, Result>
-  : IndicesReduce<Right, [...Result, TEscape<Left>]>
-  : [...Result, TEscape<Pointer>]
-type TIndices<
-  Pointer extends string,
-  Result extends string[] = Pointer extends '' ? []
-    : IndicesReduce<Pointer>
+type IndicesReduce<Pointer extends string, Result extends string[] = []> = (
+  Pointer extends `${infer Left extends string}/${infer Right extends string}` 
+    ? Left extends '' 
+      ? IndicesReduce<Right, Result>
+      : IndicesReduce<Right, [...Result, TEscape<Left>]>
+    : [...Result, TEscape<Pointer>]
+)
+type TIndices<Pointer extends string,
+  Result extends string[] = Pointer extends '' ? [] : IndicesReduce<Pointer>
 > = Result
 // ------------------------------------------------------------------
 // TResolve
 // ------------------------------------------------------------------
-type TResolve<Value extends unknown, Indices extends string[]> = Indices extends [infer Left extends string, ...infer Right extends string[]] ? Left extends keyof Value ? TResolve<Value[Left], Right>
-  : undefined
-  : Value
+type TResolve<Value extends unknown, Indices extends string[]> = (
+  Indices extends [infer Left extends string, ...infer Right extends string[]] 
+    ? Left extends keyof Value 
+      ? TResolve<Value[Left], Right>
+      : undefined
+    : Value
+)
 // ------------------------------------------------------------------
 // XPointerGet
 // ------------------------------------------------------------------
 /** Type Level RFC 6901 Json Pointer Resolver */
-export type XPointerGet<Value extends unknown, Pointer extends string, Indices extends string[] = TIndices<Pointer>, Result extends unknown = TResolve<Value, Indices>> = Result
+export type XPointerGet<Value extends unknown, Pointer extends string, 
+  Indices extends string[] = TIndices<Pointer>, 
+  Result extends unknown = TResolve<Value, Indices>
+> = Result
