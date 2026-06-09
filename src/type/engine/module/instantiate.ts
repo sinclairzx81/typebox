956eef9aaa4d57a1b84df364120e164899a3e92a
sinclair <haydn.developer@gmail.com>
2026-06-09 22:01:03 +0900

    Output Module Declarations Only

--- src/type/engine/module/instantiate.ts
+++ src/type/engine/module/instantiate.ts
@@ -45,60 +45,66 @@ import { type TInstantiateType, InstantiateType } from '../instantiate.ts'
 // ------------------------------------------------------------------
 // InstantiateCyclics
 // ------------------------------------------------------------------
-type TInstantiateCyclics<Context extends TProperties, CyclicKeys extends string[], Result extends TProperties = {
-  [Key in Extract<keyof Context, CyclicKeys[number]>]: TInstantiateCyclic<Context, Key, Context[Key]>
-}> = Result
-function InstantiateCyclics<Context extends TProperties, CyclicKeys extends string[]>
-  (context: Context, cyclicKeys: [...CyclicKeys]):
-  TInstantiateCyclics<Context, CyclicKeys> {
-  const keys = Guard.Keys(context).filter(key => cyclicKeys.includes(key))
-  return keys.reduce((result, key) => {
-    return { ...result, [key]: InstantiateCyclic(context, key, context[key]) }
+type TInstantiateCyclics<Context extends TProperties, Declarations extends TProperties, CyclicKeys extends string[], 
+  DeclarationContext extends TProperties = Memory.TAssign<Context, Declarations>,
+  Result extends TProperties = {
+    [Key in Extract<keyof Declarations, CyclicKeys[number]>]: TInstantiateCyclic<DeclarationContext, Key, Declarations[Key]>
+  }
+> = Result
+function InstantiateCyclics<Context extends TProperties, Declarations extends TProperties, CyclicKeys extends string[]>
+  (context: Context, declarations: Declarations, cyclicKeys: [...CyclicKeys]):
+  TInstantiateCyclics<Context, Declarations, CyclicKeys> {
+  const declarationContext = Memory.Assign(context, declarations)
+  const declarationKeys = Guard.Keys(declarations).filter(key => cyclicKeys.includes(key))
+  return declarationKeys.reduce((result, key) => {
+    return { ...result, [key]: InstantiateCyclic(declarationContext, key, declarations[key]) }
   }, {}) as never
 }
 // ------------------------------------------------------------------
 // InstantiateNonCyclics
 // ------------------------------------------------------------------
-type TInstantiateNonCyclics<Context extends TProperties, CyclicKeys extends string[], Result extends TProperties = {
-  [Key in Exclude<keyof Context, CyclicKeys[number]>]: TInstantiateType<Context, TState<[], []>, Context[Key]>
-}> = Result
-function InstantiateNonCyclics<Context extends TProperties, CyclicKeys extends string[]>
-  (context: Context, cyclicKeys: [...CyclicKeys]):
-  TInstantiateCyclics<Context, CyclicKeys> {
-  const keys = Guard.Keys(context).filter(key => !cyclicKeys.includes(key))
-  return keys.reduce((result, key) => {
-    return { ...result, [key]: InstantiateType(context, State([], []), context[key]) }
+type TInstantiateNonCyclics<Context extends TProperties, Declarations extends TProperties, CyclicKeys extends string[], 
+  DeclarationContext extends TProperties = Memory.TAssign<Context, Declarations>,
+  Result extends TProperties = {
+    [Key in Exclude<keyof Declarations, CyclicKeys[number]>]: TInstantiateType<DeclarationContext, TState<[], []>, Declarations[Key]>
+  }
+> = Result
+function InstantiateNonCyclics<Context extends TProperties, Declarations extends TProperties, CyclicKeys extends string[]>
+  (context: Context, declarations: Declarations, cyclicKeys: [...CyclicKeys]):
+  TInstantiateCyclics<Context, Declarations, CyclicKeys> {
+  const declarationContext = Memory.Assign(context, declarations)
+  const declarationKeys = Guard.Keys(declarations).filter(key => !cyclicKeys.includes(key))
+  return declarationKeys.reduce((result, key) => {
+    return { ...result, [key]: InstantiateType(declarationContext, State([], []), declarations[key]) }
   }, {}) as never
 }
 // ------------------------------------------------------------------
 // InstantiateModule
 // ------------------------------------------------------------------
-type TInstantiateModule<Context extends TProperties,
-  CyclicCandidates extends string[] = TCyclicCandidates<Context>,
-  InstantiatedCyclics extends TProperties = TInstantiateCyclics<Context, CyclicCandidates>,
-  InstantiatedNonCyclics extends TProperties = TInstantiateNonCyclics<Context, CyclicCandidates>,
+type TInstantiateModule<Context extends TProperties, Declarations extends TProperties,
+  CyclicCandidates extends string[] = TCyclicCandidates<Declarations>,
+  InstantiatedCyclics extends TProperties = TInstantiateCyclics<Context, Declarations, CyclicCandidates>,
+  InstantiatedNonCyclics extends TProperties = TInstantiateNonCyclics<Context, Declarations, CyclicCandidates>,
   InstantiatedModule extends TProperties = InstantiatedCyclics & InstantiatedNonCyclics
 > = { [Key in keyof InstantiatedModule]: InstantiatedModule[Key] } & {} // safe: no-key-overlap
-function InstantiateModule<Context extends TProperties>
-  (context: Context, options: TSchemaOptions):
-  TInstantiateModule<Context> {
-  const cyclicCandidates = CyclicCandidates(context) as string[]
-  const instantiatedCyclics = InstantiateCyclics(context, cyclicCandidates)
-  const instantiatedNonCyclics = InstantiateNonCyclics(context, cyclicCandidates)
+function InstantiateModule<Context extends TProperties, Declarations extends TProperties>
+  (context: Context, declarations: Declarations, options: TSchemaOptions):
+  TInstantiateModule<Context, Declarations> {
+  const cyclicCandidates = CyclicCandidates(declarations) as string[]
+  const instantiatedCyclics = InstantiateCyclics(context, declarations, cyclicCandidates)
+  const instantiatedNonCyclics = InstantiateNonCyclics(context, declarations, cyclicCandidates)
   const instantiatedModule = { ...instantiatedCyclics, ...instantiatedNonCyclics }
   return Memory.Update(instantiatedModule, {}, options) as never
 }
 // ------------------------------------------------------------------
 // Instantiate
 // ------------------------------------------------------------------
-export type TModuleInstantiate<Context extends TProperties, _State extends TState, Properties extends TProperties,
-  ModuleContext extends TProperties = Memory.TAssign<Context, Properties>,
-  InstantiatedModule extends TProperties = TInstantiateModule<ModuleContext>,
+export type TModuleInstantiate<Context extends TProperties, _State extends TState, Declarations extends TProperties,
+  InstantiatedModule extends TProperties = TInstantiateModule<Context, Declarations>,
 > = InstantiatedModule
-export function ModuleInstantiate<Context extends TProperties, State extends TState, Properties extends TProperties>
-  (context: Context, _state: State, properties: Properties, options: TSchemaOptions):
-  TModuleInstantiate<Context, State, Properties> {
-  const moduleContext = Memory.Assign(context, properties)
-  const instantiatedModule = InstantiateModule(moduleContext, options)
+export function ModuleInstantiate<Context extends TProperties, State extends TState, Declarations extends TProperties>
+  (context: Context, _state: State, declarations: Declarations, options: TSchemaOptions):
+  TModuleInstantiate<Context, State, Declarations> {
+  const instantiatedModule = InstantiateModule(context, declarations, options)
   return instantiatedModule as never
 }
