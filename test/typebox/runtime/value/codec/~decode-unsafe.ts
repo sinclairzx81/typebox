f2be6a3e29820755a785ff37a318c43ca1913449
sinclair <haydn.developer@gmail.com>
2026-06-17 23:38:29 +0900

    Handle DecodeUnsafe Unreachable Cases

---
+++ test/typebox/runtime/value/codec/~decode-unsafe.ts
@@ -0,0 +1,36 @@
+import { DecodeUnsafe } from 'typebox/value'
+import { Type } from 'typebox'
+import { Assert } from 'test'
+
+const Test = Assert.Context('Value.Codec.DecodeUnsafe')
+
+// ------------------------------------------------------------------
+// Coverage
+// ------------------------------------------------------------------
+Test('Should DecodeUnsafe 1', () => {
+  const T = Type.Object({
+    x: Type.Optional(Type.Number())
+  })
+  const D = DecodeUnsafe({}, T, undefined)
+  Assert.IsEqual(D, undefined)
+})
+Test('Should DecodeUnsafe 2', () => {
+  const T = Type.Array(Type.String())
+  const D = DecodeUnsafe({}, T, undefined)
+  Assert.IsEqual(D, undefined)
+})
+Test('Should DecodeUnsafe 3', () => {
+  const T = Type.Tuple([Type.String()])
+  const D = DecodeUnsafe({}, T, undefined)
+  Assert.IsEqual(D, undefined)
+})
+Test('Should DecodeUnsafe 4', () => {
+  const T = Type.Record(Type.String(), Type.String())
+  const D = DecodeUnsafe({}, T, undefined)
+  Assert.IsEqual(D, undefined)
+})
+Test('Should DecodeUnsafe 5', () => {
+  const T = Type.Record(Type.Number(), Type.String())
+  const D = DecodeUnsafe({}, T, { x: 1 })
+  Assert.IsEqual(D, { x: 1 })
+})
