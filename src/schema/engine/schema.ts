fd1b040894b91bded5cf7c96106dfb4d919ff02a
sinclairzx81 <haydn.developer@gmail.com>
2026-07-23 22:47:54 +0900

    Use Guards for HasTypeName

--- src/schema/engine/schema.ts
+++ src/schema/engine/schema.ts
@@ -83,7 +83,7 @@ import { BuildUniqueItems, CheckUniqueItems, ErrorUniqueItems } from './uniqueIt
 // ----------------------------------------------------------------
 function HasTypeName(schema: Schema.XSchemaObject, typename: string): boolean {
   return Schema.IsType(schema) &&
-    (G.IsArray(schema.type) && schema.type.length > 0 && schema.type.every(type => G.IsEqual(type, typename)) ||
+    (G.IsArray(schema.type) && G.IsGreaterThan(schema.type.length, 0) && G.Every(schema.type, 0, type => G.IsEqual(type, typename)) ||
       G.IsEqual(schema.type, typename))
 }
 // ----------------------------------------------------------------
