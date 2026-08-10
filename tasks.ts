422409929fd19384b94cd71e7c61376a6f9958c3
sinclairzx81 <haydn.developer@gmail.com>
2026-08-10 19:50:16 +0900

    Version

--- tasks.ts
+++ tasks.ts
@@ -9,7 +9,7 @@ import { Metrics } from './task/metrics/index.ts'
 import { Spec } from './task/spec/index.ts'
 import { Task } from 'tasksmith'
 
-const Version = '1.3.11'
+const Version = '1.3.12'
 
 // ------------------------------------------------------------------
 // Build
